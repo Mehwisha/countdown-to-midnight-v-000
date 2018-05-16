77be0536482bda594cd1a6ab55ca8cfe2a8d4dfb
Mehwish Aziz <mehwisha@msn.com>
2018-05-16 15:53:40 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -9,5 +9,11 @@ def countdown(number)
   return "HAPPY NEW YEAR!"
 end
 def countdown_with_sleep
-sleep 5
+  number = 10
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  sleep 5
+  return "HAPPY NEW YEAR!"
 end
